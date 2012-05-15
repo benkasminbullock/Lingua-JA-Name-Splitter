71ec6b66fd3abbb9e4479496587b12efafc6c22d
Ben Bullock <benkasminbullock@gmail.com>
2012-05-15 20:29:01 +0900

    Initial commit of Lingua-JA-Name-Splitter

---
+++ lib/Lingua/JA/Name/Splitter.pm
@@ -0,0 +1,13 @@
+=head1 NAME
+
+Lingua::JA::Name::Splitter
+
+=cut
+package Lingua::JA::Name::Splitter;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+use warnings;
+use strict;
+our $VERSION = 0.01;
+1;
---
+++ t/Lingua-JA-Name-Splitter.t
@@ -0,0 +1,9 @@
+use warnings;
+use strict;
+use Test::More tests => 1;
+BEGIN { use_ok('Lingua::JA::Name::Splitter') };
+use Lingua::JA::Name::Splitter;
+
+# Local variables:
+# mode: perl
+# End:
